d605dac72df42af2043b3024fbac454e417af000
Juan Carlos Caicedo R <juancarloscaicedorodriguez@gmail.com>
2022-07-16 12:54:19 -0500

    Integración con Bootstrap
    Integramos Bootstrap en nuestro proyecto Laravel

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- CSS only -->
+<link href="{{asset('public/bootstrap/bootstrap.min.css')}}" rel="stylesheet">
+
+    <title>Document</title>
+</head>
+<body>
+    <h1>Home Page</h1>
+</body>
+</html>
